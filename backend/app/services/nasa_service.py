acde6e66293a9367bcd1fce1415e5316519d5b50
Thato Mongwe <thatomongwe47@gmail.com>
2024-09-15 18:20:46 +0000

    database configurations

--- backend/app/services/nasa_service.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-API_KEY = 'your_nasa_api_key'
-
-def get_nasa_image():
-    url = f'https://api.nasa.gov/planetary/apod?api_key={API_KEY}'
-    response = requests.get(url)
-    return response.json()
--- backend/app/services/weather_service.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-API_KEY = 'your_openweathermap_api_key'
-
-def get_weather_data(city):
-    url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}'
-    response = requests.get(url)
-    return response.json()
